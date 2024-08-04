9a917f228f41597e9157894ed192f4fc058f6c8c
lypavel <157053921+lypavel@users.noreply.github.com>
2024-08-04 20:26:22 +0300

    Add multiparts archive downloading

--- server.py
+++ server.py
@@ -1,9 +1,32 @@
+import asyncio
 from aiohttp import web
 import aiofiles
+from pathlib import Path
 
 
 async def archive(request):
-    raise NotImplementedError
+    response = web.StreamResponse()
+    response.headers['Content-Disposition'] = 'attachment; \
+        filename="archive.zip"'
+    await response.prepare(request)
+
+    archive_hash = request.match_info.get('archive_hash')
+
+    process = await asyncio.create_subprocess_exec(
+        'zip',
+        '-r',
+        '-',
+        '.',
+        cwd=Path(f'test_photos/{archive_hash}'),
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+
+    while True:
+        stdout = await process.stdout.read(n=512000)
+        if process.stdout.at_eof():
+            return response
+        await response.write(stdout)
 
 
 async def handle_index_page(request):
@@ -18,4 +41,4 @@ if __name__ == '__main__':
         web.get('/', handle_index_page),
         web.get('/archive/{archive_hash}/', archive),
     ])
-    web.run_app(app)
+    web.run_app(app, host='0.0.0.0', port=8000)
